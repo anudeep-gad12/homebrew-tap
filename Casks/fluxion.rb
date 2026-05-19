8e1296ba6d531e9cb9114929b67d11c00f2e2532
Anudeep Gad <69517148+anudeep-gad12@users.noreply.github.com>
2026-05-19 12:19:21 +0530

    Update Fluxion cask to v0.1.2

--- Casks/fluxion.rb
+++ Casks/fluxion.rb
@@ -1,6 +1,6 @@
 cask "fluxion" do
-  version "0.1.1"
-  sha256 "06c77dd7d81800c2c67ac3999b01c3dcf013c5a62d5e9e2420b64cd155c2c255"
+  version "0.1.2"
+  sha256 "c7dc99c4d9ad84186293bcd985fcf9e1eb018b94571421caa85ef5cece3aae23"
 
   url "https://github.com/anudeep-gad12/Fluxion/releases/download/v#{version}/Fluxion-macos-arm64.zip"
   name "Fluxion"
